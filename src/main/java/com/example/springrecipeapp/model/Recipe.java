592ce0883c1843c385bdc0d4d2f5d74a3720b745
nubranger <mtviraga@gmail.com>
2020-08-17 22:56:51 +0300

    added model

--- src/main/java/com/example/springrecipeapp/model/Recipe.java
+++ src/main/java/com/example/springrecipeapp/model/Recipe.java
@@ -1,6 +1,7 @@
 package com.example.springrecipeapp.model;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -15,9 +16,21 @@ public class Recipe {
     private Integer servings;
     private String source;
     private String url;
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
     private String directions;
     //todo add
     //private Difficulty difficulty;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
